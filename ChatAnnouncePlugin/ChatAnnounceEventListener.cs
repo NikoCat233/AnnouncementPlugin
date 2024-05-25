0a23fa232f2a5464f7697e4f2233be5671f63897
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-05-25 23:40:16 +0800

    lol

--- ChatAnnouncePlugin/ChatAnnounceEventListener.cs
+++ ChatAnnouncePlugin/ChatAnnounceEventListener.cs
@@ -3,11 +3,6 @@ using Impostor.Api.Events;
 using Microsoft.Extensions.Logging;
 using Impostor.Api.Net.Inner.Objects;
 using System.Text;
-using System.Text.Json;
-using Impostor.Api.Innersloth;
-using Impostor.Hazel.Abstractions;
-using Impostor.Api.Innersloth.Customization;
-using System;
 
 namespace ChatAnnouncePlugin
 {
@@ -60,98 +55,22 @@ namespace ChatAnnouncePlugin
         [EventListener]
         public async void OnPlayerChat(IPlayerChatEvent e)
         {
-            string input = e.Message;
-            bool startsWithPrefix = e.Message.StartsWith("/") || e.Message.StartsWith("?");
+            string input = e.Message.TrimStart('\n', '\r');
+            bool startsWithPrefix = input.StartsWith('?');
             if (!startsWithPrefix) return;
-            if (e.Message.Length < 2) return;
+            if (input.Length < 2) return;
             e.IsCancelled = true;
-            List<string> slashcommands = new List<string>{ "/timer", "/help", "/rules","/cancel","/end"};
-            List<string> altCommands = new List<string> { "?timer", "?help", "?rules", "?end", "?cancel" };
-            List<string> impchatCommands = new List<string> { "//", "??", "/w", "?w", "/say", "?say" };
             string command = input.Split(" ")[0].Trim().ToLower();
-            string doublePrefix = input.Substring(0, 2);
-            string message = "";
-            bool isCommand = false;
 
-            if (slashcommands.Contains(command) || altCommands.Contains(command) || impchatCommands.Contains(command)) {
-                isCommand = true;
-            }
-            if(doublePrefix.Equals("//") || doublePrefix.Equals("??"))
+            if (command.Equals("?help"))
             {
-                isCommand = true;
-            }
-            if (!isCommand) 
-            {
-                message = _config.wrongCommandMessage;
+                string message = _config.helpMessage;
                 await e.PlayerControl.SendChatToPlayerAsync(message);
-                return;
             }
-
-            if (impchatCommands.Contains(command) || impchatCommands.Contains(doublePrefix))
+            else
             {
                 e.IsCancelled = false;
-                return;
             }
-
-            if (input.Split(" ").Length > 1 && (command.Equals("/help") || command.Equals("?help")))
-            {
-                
-                switch (e.Message.Split(" ")[1].Trim().ToLower())
-                {
-                    case "timer":
-                        message = _config.timerMessage;
-                        break;
-                    case "help":
-                        message = _config.helpMessage;
-                        break;
-                    case "rules":
-                        message = _config.rulesMessage;
-                        break;
-                    case "cancel":
-                        message = _config.cancelMessage;
-                        break;
-                    case "say":
-                        message = _config.impChatMessage;
-                        break;
-                    case "w":
-                        message = _config.impChatMessage;
-                        break;
-                    case "//":
-                        message = _config.impChatMessage;
-                        break;
-                    case "??":
-                        message = _config.impChatMessage;
-                        break;
-                    default:
-                        message = _config.wrongCommandMessage;
-                        break;
-                } 
-            } else
-            {
-                switch (command)
-                {
-                    case "/help":
-                        message = _config.helpMessage;
-                        break;
-                    case "?help":
-                        message = _config.helpMessage;
-                        break;
-                    case "/rules":
-                        message = _config.rulesMessage;
-                        break;
-                    case "?rules":
-                        message = _config.rulesMessage;
-                        break;
-                    default:
-                        message = "";
-                        break;
-                }
-            }
-            if (message == "") {
-                e.IsCancelled = false;
-                return;
-            }
-            await e.PlayerControl.SendChatToPlayerAsync(message);
         }
 
         private void sendMessage(IInnerPlayerControl player, string message = "") 
